f2bf47a4d6d83b6fa7330cbb08bdbca1aac6d8cd
Luan Eduardo da Costa <luan1346@gmail.com>
2021-06-08 16:57:06 -0300

    Make the create command works with directories

--- src/index.ts
+++ src/index.ts
@@ -17,20 +17,62 @@ program
     const templatesDir = path.resolve(process.cwd(), '__file-templates__')
     const sourcePath = path.resolve(templatesDir, source)
 
-    const isSourcePathDirectory = fs.lstatSync(sourcePath).isDirectory()
-    const isSourcePathFile = fs.lstatSync(sourcePath).isFile()
+    const templatesDirExists = fs.existsSync(templatesDir)
+    if (!templatesDirExists) {
+      console.log(
+        chalk.red(
+          'The __file-templates__ directory does not exist in your project root',
+        ),
+      )
+    }
+
+    const sourcePathExists = fs.existsSync(sourcePath)
+    if (!sourcePathExists) {
+      console.log(chalk.red(`${sourcePathExists} not found`))
+    }
+
+    const fsStatus = fs.lstatSync(sourcePath)
+    const isSourcePathDirectory = fsStatus.isDirectory()
+    const isSourcePathFile = fsStatus.isFile()
 
     if (isSourcePathDirectory) {
-      console.log(chalk.red('This functionality is not available yet'))
+      const directoryFiles = fs.readdirSync(sourcePath)
+
+      if (directoryFiles.length === 0) {
+        console.log(chalk.red(`No files found at ${sourcePath}`))
+        return
+      }
+
+      const directoryParts = source.split('/')
+      const directoryName = directoryParts[directoryParts.length - 1]
+      const destinationPath = path.resolve(
+        process.cwd(),
+        destination,
+        directoryName,
+      )
+
+      fs.mkdirSync(destinationPath)
+
+      directoryFiles.forEach((fileName) => {
+        const fileDestinationPath = path.resolve(
+          process.cwd(),
+          destinationPath,
+          fileName,
+        )
+
+        const filePath = path.resolve(sourcePath, fileName)
+        const fileContent = fs.readFileSync(filePath, 'utf-8')
+        fs.writeFileSync(fileDestinationPath, fileContent)
+        console.log(chalk.green(`File created at ${fileDestinationPath}`))
+      })
+
       return
     } else if (isSourcePathFile) {
       const sourceParts = source.split('/')
       const fileName = sourceParts[sourceParts.length - 1]
       const destinationPath = path.resolve(process.cwd(), destination, fileName)
-
       const fileContent = fs.readFileSync(sourcePath, 'utf-8')
       fs.writeFileSync(destinationPath, fileContent)
-
       console.log(chalk.green(`File created at ${destinationPath}`))
       return
     }
