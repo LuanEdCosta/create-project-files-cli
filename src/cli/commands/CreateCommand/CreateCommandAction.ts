20dca6945b832a3d767ceae001c3e3b629d0f72b
Luan Eduardo da Costa <luan1346@gmail.com>
2021-07-30 23:55:58 -0300

    Create separate function to log the create command results

--- src/cli/commands/CreateCommand/CreateCommandAction.ts
+++ src/cli/commands/CreateCommand/CreateCommandAction.ts
@@ -1,6 +1,19 @@
 import chalk from 'chalk'
 
-import { CreateCommand, CreateCommandOptions } from '@app/core/commands'
+import {
+  CreateCommand,
+  CreateCommandResult,
+  CreateCommandOptions,
+} from '@app/core/commands'
+
+export const showResultsInConsole = (results: CreateCommandResult[]) => {
+  results.forEach((result) => {
+    const isFile = result.type === 'file'
+    const typeText = isFile ? 'File' : 'Folder'
+    const message = `${typeText} created at ${result.destinationPath}`
+    console.log(chalk.green(message))
+  })
+}
 
 export const createCommandAction = (
   source: string,
@@ -8,11 +21,5 @@ export const createCommandAction = (
   options: CreateCommandOptions,
 ) => {
   const results = new CreateCommand(source, destination, options).run()
-
-  results.forEach((result) => {
-    const isFile = result.type === 'file'
-    const typeText = isFile ? 'File' : 'Folder'
-    const message = `${typeText} created at ${result.destinationPath}`
-    console.log(chalk.green(message))
-  })
+  showResultsInConsole(results)
 }
