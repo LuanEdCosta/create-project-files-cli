bffa10347c5e7f4d1dfa6badd3ac910e4d93f24f
Luan Eduardo da Costa <luan1346@gmail.com>
2021-08-06 21:45:11 -0300

    Export MisusedOptionsError in the root index.ts file

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,12 @@ export {
   ResultTypes,
 } from './core/commands'
 
-export { NotFoundError, SyntaxError, SyntaxErrorOptions } from './core/errors'
+export {
+  NotFoundError,
+  SyntaxError,
+  SyntaxErrorOptions,
+  MisusedOptionsError,
+  MisusedOptionsErrorOptions,
+} from './core/errors'
 
 export { CreateCommand as default } from './core/commands'
