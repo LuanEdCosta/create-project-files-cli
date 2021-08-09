28b462bb28c16bb4f424bf9b6f233f5a33ea2ab5
Luan Eduardo da Costa <luan1346@gmail.com>
2021-08-08 21:08:50 -0300

    Create the escapeRegex function

--- src/core/utils/TextUtils.ts
+++ src/core/utils/TextUtils.ts
@@ -1,7 +1,13 @@
+export const escapeRegex = (str: string): string => {
+  return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+}
+
 export const replaceTextPieces = (
   text: string,
   pieces: { [key: string]: string },
 ): string => {
-  const regex = new RegExp(Object.keys(pieces).join('|'), 'g')
+  const escapedPieces = Object.keys(pieces).map((piece) => escapeRegex(piece))
+  const pattern = escapedPieces.join('|')
+  const regex = new RegExp(pattern, 'g')
   return text.replace(regex, (matched) => pieces[matched])
 }
